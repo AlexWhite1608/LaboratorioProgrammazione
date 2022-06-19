50726f964da71e916d5672b1a5199d28b73707dd
Alessandro <alessandro.bianco1608@gmail.com>
2022-06-19 21:39:51 +0200

    fix bug sovrascrittura della parola quando si aggiunge il prodotto al carrello

--- itemdelegatepaint.cpp
+++ itemdelegatepaint.cpp
@@ -1,5 +1,6 @@
 #include "itemdelegatepaint.h"
 #include <QAbstractItemModel>
+#include <QApplication>
 
 ItemDelegatePaint::ItemDelegatePaint(QObject *parent)
 {
@@ -8,22 +9,28 @@ ItemDelegatePaint::ItemDelegatePaint(QObject *parent)
 /* Colora la riga selezionata */
 void ItemDelegatePaint::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
+
     if(paintRequest && indexVector.contains(index)) {
         foreach(QModelIndex i, indexVector){
-            painter->fillRect(option.rect, QColor(0,255,0));
+            if(index == i){
+                painter->fillRect(option.rect, QColor(0,255,0));
 
-            if( option.state & QStyle::State_Selected )
-            {
-                painter->fillRect(option.rect, option.palette.highlight());
+                if( option.state & QStyle::State_Selected )
+                {
+                    painter->fillRect(option.rect, option.palette.highlight());
+                }
+
+                painter->drawText(option.rect, Qt::AlignCenter, i.data().toString());
+                QStyledItemDelegate::paint(painter, option, i);
+
+                //TODO: aggiornare il contatore di quanti elementi sono nel carrello (signal)
+
             }
-
-            QStyledItemDelegate::paint(painter, option, i);
-
         }
-
-        //manda segnale che imposta il paintRequest a false
     }
+
     else
+
     {
         painter->fillRect(option.rect, QColor(223, 241, 255));
 
